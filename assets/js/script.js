9dc16f1be95b29926ae5be96c8961f4d81a1d45e
AWSMProdigy <StraightAKyle@gmail.com>
2021-09-27 23:56:51 -0400

    Most javascript done, css needed

--- assets/js/script.js
+++ assets/js/script.js
@@ -59,26 +59,36 @@ if(localStorage.getItem("scores") == null){
     getscores();
 }
 
+
 var myTimer = document.querySelector("#theTimer");
 var gameOver;
 var startButton = document.querySelector("#starto");
+var resetButton = document.querySelector("#reseto");
 var questionDisplay = document.querySelector("#question");
 var aDisplay = document.querySelector("#answerA");
 var bDisplay = document.querySelector("#answerB");
 var cDisplay = document.querySelector("#answerC");
 var dDisplay = document.querySelector("#answerD");
+var answerButtons = document.querySelectorAll(".answers");
 var resultsDisplay = document.querySelector("#results");
-var scoreDisplay = document.querySelector("#scores");
+var gameDisplay = document.querySelector("#gameDisplay");
+var highScoreList = document.querySelector("#highScore");
+var submitScoreBtn = document.querySelector("#submitScore");
+var myForm = document.querySelector("#myForm");
+var input = myForm.elements['nickName'];
 var secondsLeft = 60;
 var current = 0;
+var timerInterval;
+var total;
+
+myForm.style.visibility = "hidden";
 
 function storescores(){
     localStorage.setItem("scores", JSON.stringify(scores));
 }
   
-  function getscores(){
+function getscores(){
     var tempscores = JSON.parse(localStorage.getItem("scores"));
-    console.log(tempscores);
   
     if(tempscores !== null || tempscores !== undefined){
       scores = tempscores;
@@ -86,53 +96,75 @@ function storescores(){
 }
 
 function startGame(){
+    clearInterval(timerInterval);
+    secondsLeft = 60;
+    current = 0;
     gameOver = false;
+    total = 0;
     setTime();
-    aDisplay.style.visibility = "visible";
-    bDisplay.style.visibility = "visible";
-    cDisplay.style.visibility = "visible";
-    dDisplay.style.visibility = "visible";
-    startButton.style.visibilty = "hidden";
+    for (var i = 0; i < answerButtons.length; i++) {
+        answerButtons[i].style.visibility = "visible";
+    }
+    gameDisplay.style.visibility = "visible";
+    startButton.style.Display = "none";
+    console.log(startDiv.style.visibilty);
+    resultsDisplay.style.visibility = "hidden";
+    myForm.style.visibility = "hidden";
+    
     showQuestions(current);
 }
 
 function endGame(result){
+    myForm.style.visibility = "visible";
+    resultsDisplay.style.visibility = "visible";
+    for (var i = 0; i < answerButtons.length; i++) {
+        answerButtons[i].style.visibility = "hidden";
+    }
+    total += secondsLeft;
     gameOver = true;
-    console.log("Game over!");
     if(result === true){
         questionDisplay.textContent = "You win! Play again?"
     }
     else{
         questionDisplay.textContent = "You lose! Play again?"
-    }
-    var outcome = {"name" : "xxx", "score": 69};
-    scores.push(outcome);
-    scoreDisplay.innterText = scores[scores.length];
-    storescores();
+    }    
+}
 
+function postScore(name){
+    gameDisplay.style.visibility = "hidden";
+    myForm.style.visibility = "hidden";
+    var newScore = {name, total};
+    scores.push(newScore);
+    var child = highScoreList.lastElementChild;
+    while(child){
+        highScoreList.removeChild(child);
+        child = highScoreList.lastElementChild;
+    }
+    for(var i = 0; i < scores.length; i++){
+        newLI = document.createElement("li");
+        newLI.innerHTML = "Name: " + Object.values(scores[i])[0] + " Score: " + Object.values(scores[i])[1];
+        highScoreList.append(newLI);
+    }
+    storescores();
 }
 
 function showQuestions(number){
-    if(questionBank[number] !== undefined){
+    startButton.style.visibilty = "hidden";
+
         questionDisplay.textContent = questionBank[number].question;
         aDisplay.textContent = questionBank[number].answers.a;
         bDisplay.textContent = questionBank[number].answers.b;
         cDisplay.textContent = questionBank[number].answers.c;
         dDisplay.textContent = questionBank[number].answers.d;
-    }
+    
 }
 
 function setTime() {
     myTimer.textContent = secondsLeft + " seconds left.";
-    var timerInterval = setInterval(function() {
+    timerInterval = setInterval(function() {
         secondsLeft--;
         myTimer.textContent = secondsLeft + " seconds left.";
-
-        if(current === questionBank.length) {
-            clearInterval(timerInterval);
-            endGame(true);
-        }
-        else if(secondsLeft === 0){
+        if(secondsLeft === 0){
             clearInterval(timerInterval);
             endGame(false);
         }
@@ -143,21 +175,41 @@ function setTime() {
 function checkAnswer(input){
     if(!gameOver){
         console.log(input);
-        if(input === questionBank[current].correct){
-
-        }
-        else{
+        if(input !== questionBank[current].correct){
             secondsLeft = secondsLeft - 5;
         }
+        else if(input === questionBank[current].correct){
+            total += 5;
+        }
+        if(current + 1 === questionBank.length) {
+            clearInterval(timerInterval);
+            endGame(true);
+            return;
+        }
         showQuestions(++current);
+    }
+}
+
+function resetScores(){
+    if(gameOver){
+        localStorage.clear();
+        var child = highScoreList.lastElementChild;
+        while(child){
+        highScoreList.removeChild(child);
+        child = highScoreList.lastElementChild;
+        }
     }
 }
 
 
 
 
-
+myForm.addEventListener('submit', function(event){
+    event.preventDefault();
+    postScore(input.value);
+})
 startButton.addEventListener("click", startGame);
+resetButton.addEventListener("click", resetScores);
 aDisplay.addEventListener("click", function(){
     checkAnswer("a")
 });
